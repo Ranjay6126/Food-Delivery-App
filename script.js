c1274b0ed765e64f1a78e4946abddfc18d61020a
Ranjay6126 <panditranjay33@gmail.com>
2025-11-15 01:46:33 +0530

    added slider

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
  var swiper = new Swiper(".mySwiper", {
+      loop: true,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#prev",
+        prevEl: "#next",
       },
     });
